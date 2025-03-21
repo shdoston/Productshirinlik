ced33a29e36712065092431c0bf334bac764dcdb
Dostonbek <dostonbek200gmail.com>
2025-03-21 07:48:16 +0500

    desertsProduct

--- src/components/DessertsList.jsx
+++ src/components/DessertsList.jsx
@@ -3,7 +3,7 @@ import { Dessert } from "./";
 function DessertList({ desserts }) {
   return (
     <main>
-      <h1 className="dessert-title">Uyquchi shirinliklari</h1>
+      <h1 className="dessert-title">Desserts</h1>
       <div className="Desertlist-container">
         {desserts.map((d) => {
           return <Dessert key={d.id} d={d} />;
